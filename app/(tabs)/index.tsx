bd3cf5c798e9a3eff67025dab3ed72d8c32bce90
AlexanderBertilsson <alexander.bertilsson@hobbitsolutions.se>
2024-11-10 00:54:25 +0100

    modal-part1

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,42 @@
 import {  View, StyleSheet } from "react-native";
-
+import { useState } from "react";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
+import * as ImagePicker from 'expo-image-picker';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if(!result.canceled) {
+      setSelectedImage(result.assets[0].uri)
+      setShowAppOptions(true);
+    } else{
+      alert('You did not select any image.')
+    }
+  }
+
   return (
     <View style={styles.contaier}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage}/>
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage}/>
       </View>
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo"/>
-        <Button label="Use this photo"/>
+      {showAppOptions ? (
+        <View/>
+      ) : (
+        <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
+        <Button label="Use this photo" onPress={() => setShowAppOptions(true)}/>
       </View>
+      )}
     </View>
   );
 }
